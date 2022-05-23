9224410ea7a62d2889cab97475b2915a15c88d34
Kent <kentsday@outlook.com>
2022-05-23 07:39:08 +0000

    1

--- lib/src/types.d.ts
+++
@@ -1,8 +0,0 @@
-export declare type CanisterIdString = string;
-export declare type NeuronId = bigint;
-export declare type AccountIdentifier = string;
-export declare type BlockHeight = bigint;
-export declare type E8s = bigint;
-export declare type Memo = bigint;
-export declare type PrincipalString = string;
-export declare type SubAccount = Uint8Array;
