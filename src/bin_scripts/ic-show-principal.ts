0275f02140debaf56f550c7484d78a61363979a9
Kent <92845901+kentsday@users.noreply.github.com>
2022-05-24 23:27:11 +0800

    show-principal (#16)

---
+++ src/bin_scripts/ic-show-principal.ts
@@ -0,0 +1,9 @@
+import { exec } from "shelljs";
+import fs from "fs";
+import logger from "node-color-log";
+import { identityFactory } from "../src/identity";
+
+export const execute_task_show_principal = () => {
+    identityFactory.printIdentitys();
+    logger.info("execute_task_show_principal done");
+}
