97dc6ed65d9a64103e5ba71997d6bf7965ccb0fb
ne8u14 <101264439+ne8u14@users.noreply.github.com>
2022-08-18 15:47:23 +0800

    download did with character escaping support (#101)
    Co-authored-by: vanilla <104433129+extabr@users.noreply.github.com>

--- example/canisters/one_way_caller/src/actor.rs
+++ example/canisters/one_way_caller/src/actor.rs
@@ -1,9 +1,8 @@
-use std::future::Future;
-
 use candid::{candid_method, decode_args, encode_args, Principal};
-use ic_cdk::{api, trap};
-use ic_cdk::api::call::{call_raw, CallResult};
+use ic_cdk::api;
+use ic_cdk::api::call::call_raw;
 use ic_cdk_macros::*;
+use crate::nft::{Fungible, FungibleUser, Metadata, NonFungible, TokenIdentifier};
 
 #[update(name = "test_one_way_caller")]
 #[candid_method(update)]
@@ -25,4 +24,32 @@ pub fn test_one_way_caller(ps: Principal, target: Principal) {
     // let result: CallResult<()> = ic_cdk::call(target.clone(), "test_one_way", (ps, )).await;
     // result.unwrap();
     api::print(format!("one way result is returned"));
+}
+#[query(name = "metadataNonFungible")]
+#[candid_method(query)]
+fn metadata_non_fungible(token: TokenIdentifier) -> Metadata {
+    let metadata = Metadata::NonFungible(NonFungible {
+        metadata: Some("test".as_bytes().to_vec()),
+    });
+    metadata
+}
+#[query(name = "metadataFungible")]
+#[candid_method(query)]
+fn metadata_fungible(token: TokenIdentifier) -> Metadata {
+    let metadata = Metadata::Fungible(Fungible{
+        name : FungibleUser::Principal(Principal::anonymous()),
+        symbol: Principal::anonymous(),
+        decimals: TokenIdentifier {
+            value: "0".to_string(),
+        },
+        metadata: Some("test".as_bytes().to_vec()),
+    });
+    metadata
+}
+
+candid::export_service!();
+#[query(name = "__get_candid_interface_tmp_hack")]
+#[candid_method(query, rename = "__get_candid_interface_tmp_hack")]
+fn __export_did_tmp_() -> String {
+    __export_service()
 }
---
+++ example/canisters/one_way_caller/src/nft.rs
@@ -0,0 +1,36 @@
+
+use candid::{CandidType, Deserialize, Principal};
+
+#[derive(CandidType, Debug, Clone, Deserialize)]
+pub enum   Metadata {
+    #[serde(rename = "fungible")]
+    Fungible(Fungible),
+    #[serde(rename = "nonfungible")]
+    NonFungible(NonFungible),
+}
+
+#[derive(CandidType, Debug, Clone, Deserialize)]
+pub struct Fungible {
+    pub name: FungibleUser,
+    pub symbol: Principal,
+    pub decimals: TokenIdentifier,
+    pub metadata: Option<Vec<u8>>,
+}
+
+// A user can be any principal or canister, which can hold a balance
+#[derive(CandidType, Debug, Clone, Deserialize)]
+pub enum FungibleUser {
+    #[serde(rename = "address")]
+    Address(String),
+    #[serde(rename = "principal")]
+    Principal(Principal),
+}
+#[derive(CandidType, Debug, Clone, Deserialize)]
+pub struct NonFungible {
+    pub metadata: Option<Vec<u8>>,
+}
+
+#[derive(Deserialize, CandidType, Clone, Hash, Eq, PartialEq, Debug)]
+pub struct TokenIdentifier {
+    pub value: String,
+}
